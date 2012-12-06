1de6709007190c384a7bbfb7c7bc8631e2b90ce5
Strilanc <craig.gidney@gmail.com>
2012-12-06 05:49:32 -0400

    Tested DisposableLifetime and added a ToString to it

--- Lifetime/DisposableLifetime.cs
+++ Lifetime/DisposableLifetime.cs
@@ -1,17 +1,23 @@
 using System;
+using System.Diagnostics;
 
 namespace TwistedOak.Util {
     /// <summary>
     /// Exposes a lifetime that permanently transitions from mortal to dead when the managing DisposableLifetime instance is disposed.
     /// When a DisposableLifetime is garbage collected without being disposed, the exposed lifetime becomes immortal.
     /// </summary>
+    [DebuggerDisplay("{ToString()}")]
     public sealed class DisposableLifetime : IDisposable {
         private readonly LifetimeSource _source = new LifetimeSource();
         ///<summary>The lifetime that transitions from mortal to dead when the managing DisposableLifetime is disposed.</summary>
-        public Lifetime Lifetime { get { return this._source.Lifetime; } }
+        public Lifetime Lifetime { get { return _source.Lifetime; } }
         ///<summary>Transitions the exposed lifetime from mortal to dead.</summary>
         public void Dispose() {
             _source.EndLifetime();
+        }
+        ///<summary>Returns a text representation of the disposable lifetime's current state.</summary>
+        public override string ToString() {
+            return _source.ToString();
         }
     }
 }
---
+++ LifetimeTest/Lifetime/DisposableLifetimeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwistedOak.Util;
+
+[TestClass]
+public class DisposableLifetimeTest {
+    private static readonly Func<Action> InvalidCallbackMaker = () => {
+        var r = new object();
+        return () => {
+            if (r == new object()) throw new Exception();
+            throw new Exception();
+        };
+    };
+
+    [TestMethod]
+    public void DisposableLifetime() {
+        var d = new DisposableLifetime();
+        d.Lifetime.IsMortal.AssertIsTrue();
+        d.Dispose();
+        d.Lifetime.IsDead.AssertIsTrue();
+
+        InvalidCallbackMaker.AssertCollectedAfter(a => {
+            var r = new DisposableLifetime();
+            var life = r.Lifetime;
+            life.WhenDead(a);
+            return life;
+        });
+    }
+}
