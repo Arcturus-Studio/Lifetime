7feaebba81f72aa440b15852ba30ecd92e73ea32
Strilanc <craig.gidney@gmail.com>
2012-12-08 14:45:50 -0400

    Removed intersection methods (result unstable due to float errors in some cases) - Use only distance methods

--- ExampleTest/Mathematics/GeometryTest.cs
+++ ExampleTest/Mathematics/GeometryTest.cs
@@ -5,7 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 
 [TestClass]
-public class UnitTest1 {
+public class GeometryTest {
     private static readonly double Eps = 0.0001;
     private static readonly Point P00 = new Point(0, 0);
     private static readonly Point P01 = new Point(0, 1);
@@ -27,40 +27,6 @@ public class UnitTest1 {
         var poly = Tri0UxUy;
         poly.Corners.AssertSequenceEquals(P00, P10, P01);
         poly.Edges.AssertSequenceEquals(P00.To(P10), P10.To(P01), P01.To(P00));
-    }
-
-    [TestMethod]
-    public void LineIntersectsLine() {
-        P00.To(P10).Intersects(P01.To(P11)).AssertIsFalse();
-        P00.To(P01).Intersects(P10.To(P11)).AssertIsFalse();
-
-        P00.To(P11).Intersects(P10.To(P01)).AssertIsTrue();
-        P10.To(P01).Intersects(P00.To(P11)).AssertIsTrue();
-        P00.To(P11).Intersects(P01.To(P10)).AssertIsTrue();
-    }
-    [TestMethod]
-    public void PointIntersectsPolygon() {
-        Tri0UxUy.Corners.All(e => e.Intersects(Tri0UxUy)).AssertIsTrue();
-        new Point(0.2, 0.2).Intersects(Tri0UxUy).AssertIsTrue();
-        new Point(0.8, 0.8).Intersects(Tri0UxUy).AssertIsFalse();
-        new Point(-0.2, 0.2).Intersects(Tri0UxUy).AssertIsFalse();
-    }
-    [TestMethod]
-    public void LineIntersectsPolygon() {
-        // along side
-        Tri0UxUy.Edges.All(e => e.Intersects(Tri0UxUy)).AssertIsTrue();
-        // in to out
-        new Point(0.2, 0.2).To(new Point(1, 1)).Intersects(Tri0UxUy).AssertIsTrue();
-        // out to in
-        new Point(1, 1).To(new Point(0.2, 0.2)).Intersects(Tri0UxUy).AssertIsTrue();
-        // across
-        new Point(1, 1).To(new Point(-1, -1)).Intersects(Tri0UxUy).AssertIsTrue();
-        // in
-        new Point(0.2, 0.2).To(new Point(0.4, 0.4)).Intersects(Tri0UxUy).AssertIsTrue();
-        // near
-        new Point(0.6, 0.6).To(new Point(0.6, 0.7)).Intersects(Tri0UxUy).AssertIsFalse();
-        // far
-        new Point(2, 2).To(new Point(3, 4)).Intersects(Tri0UxUy).AssertIsFalse();
     }
 
     [TestMethod]
@@ -93,6 +59,23 @@ public class UnitTest1 {
         new Point(2, 0).DistanceTo(Tri0UxUy).AssertEquals(1, Eps);
         // near edge
         new Point(0.6, 0.6).DistanceTo(Tri0UxUy).AssertEquals(0.1 * Math.Sqrt(2), Eps);
+    }
+    [TestMethod]
+    public void LineDistanceToLine() {
+        // crossing
+        P00.To(P11).DistanceTo(P10.To(P01)).AssertEquals(0);
+        P10.To(P01).DistanceTo(P00.To(P11)).AssertEquals(0);
+        P00.To(P11).DistanceTo(P01.To(P10)).AssertEquals(0);
+
+        // parallel side-by-side
+        P00.To(P10).DistanceTo(P01.To(P11)).AssertEquals(1, Eps);
+        P00.To(P01).DistanceTo(P10.To(P11)).AssertEquals(1, Eps);
+
+        // parallel on same line
+        var p22 = new Point(2, 2);
+        P00.To(p22).DistanceTo(P11.To(new Point(3, 3))).AssertEquals(0, Eps);
+        P00.To(P11).DistanceTo(p22.To(new Point(3, 3))).AssertEquals(Math.Sqrt(2), Eps);
+        P00.To(P11).DistanceTo(P11.To(p22)).AssertEquals(0, Eps);
     }
     [TestMethod]
     public void LineDistanceToPolygon() {
--- LifetimeExample2/Mathematics/GeometryUtilities.cs
+++ LifetimeExample2/Mathematics/GeometryUtilities.cs
@@ -23,33 +23,6 @@ namespace LifetimeExample.Mathematics {
             return new LineSegment(p1, p2);
         }
 
-        public static bool Intersects(this LineSegment line1, LineSegment line2) {
-            var d = line2.Start - line1.Start;
-            var c = line1.Delta.Cross(line2.Delta);
-            var t1 = d.Cross(line2.Delta) / c;
-            var t2 = d.Cross(line1.Delta) / c;
-            return 0 <= t1 && t1 <= 1
-                && 0 <= t2 && t2 <= 1;
-        }
-        public static bool Intersects(this Point point, ConvexPolygon polygon) {
-            return polygon.Edges
-                .Select(e => (int)e.Delta.Cross(point - e.Start).Sign())
-                .Where(e => e != 0)
-                .Distinct()
-                .Count() < 2;
-        }
-        public static bool Intersects(this LineSegment line, ConvexPolygon polygon) {
-            if (line.Start.Intersects(polygon)) return true;
-            if (line.End.Intersects(polygon)) return true;
-            return polygon.Edges.Any(e => line.Intersects(e));
-        }
-        public static bool Intersects(this ConvexPolygon polygon, Point point) {
-            return point.Intersects(polygon);
-        }
-        public static bool Intersects(this ConvexPolygon polygon, LineSegment line) {
-            return line.Intersects(polygon);
-        }
-
         public static double DistanceTo(this Point point, Point other) {
             return (point - other).Length;
         }
@@ -60,17 +33,31 @@ namespace LifetimeExample.Mathematics {
             if (s > 1) return point.DistanceTo(line.End);
             return offset.PerpOnto(line.Delta).Length;
         }
-        public static double DistanceTo(this LineSegment line, LineSegment other) {
-            if (line.Intersects(other)) return 0;
+        public static double DistanceTo(this LineSegment line, LineSegment line2) {
+            // do they intersect?
+            var c = line.Delta.Cross(line2.Delta);
+            var d = line2.Start - line.Start;
+            var t1 = d.Cross(line2.Delta)/c;
+            var t2 = d.Cross(line.Delta)/c;
+            if (0 <= t1 && t1 <= 1 && 0 <= t2 && t2 <= 1)
+                return 0;
+
+            // otherwise one of their endpoints is as close to the other as any other point
             return new[] {
-                line.End.DistanceTo(other),
-                line.Start.DistanceTo(other),
-                other.End.DistanceTo(line),
-                other.Start.DistanceTo(line)
+                line.End.DistanceTo(line2),
+                line.Start.DistanceTo(line2),
+                line2.End.DistanceTo(line),
+                line2.Start.DistanceTo(line)
             }.Min();
         }
         public static double DistanceTo(this Point point, ConvexPolygon polygon) {
-            if (Intersects(point, polygon)) return 0;
+            // inside?
+            if (polygon.Edges.Select(e => (int)e.Delta.Cross(point - e.Start).Sign())
+                             .Where(e => e != 0)
+                             .Distinct()
+                             .Count() < 2) 
+                return 0;
+            // when outside, one of the edges is closest
             return polygon.Edges.Select(e => point.DistanceTo(e)).Min();
         }
         public static double DistanceTo(this LineSegment line, Point point) {
@@ -80,8 +67,12 @@ namespace LifetimeExample.Mathematics {
             return point.DistanceTo(polygon);
         }
         public static double DistanceTo(this LineSegment line, ConvexPolygon polygon) {
-            if (line.Intersects(polygon)) return 0;
-            return polygon.Edges.Select(e => line.DistanceTo(e)).Min();
+            return new[] {
+                // use distance 0 if an endpoint is inside the polygon
+                line.Start.DistanceTo(polygon),
+                // otherwise use how close to the sides the line gets
+                polygon.Edges.Select(e => line.DistanceTo(e)).Min()
+            }.Min();
         }
         public static double DistanceTo(this ConvexPolygon polygon, LineSegment line) {
             return line.DistanceTo(polygon);
