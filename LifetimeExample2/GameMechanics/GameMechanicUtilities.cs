3cc8b834a9f6d256a8b61de7fd6983aa39d5fbb7
Strilanc <craig.gidney@gmail.com>
2012-12-08 20:25:00 -0400

    MainWindow is now reasonable

--- LifetimeExample2/GameMechanics/GameMechanicUtilities.cs
+++ LifetimeExample2/GameMechanics/GameMechanicUtilities.cs
@@ -5,6 +5,69 @@ using TwistedOak.Util;
 
 namespace LifetimeExample2 {
     public static class GameMechanicUtilities {
+        public static Ball SpawnBall(this Game game, Ball parent) {
+            // generate an angle that's not too aligned with either the X or Y axies
+            var omit = 0.1;
+            var quarterTurnet = Math.PI / 2;
+            var theta = game.Rng.NextDouble() * quarterTurnet * (1 - 2 * omit)
+                      + quarterTurnet * omit
+                      + game.Rng.Next(4) * quarterTurnet;
+
+            // create the child ball
+            var ball = new Ball {
+                Pos = parent.Pos,
+                LastPos = parent.Pos,
+                Radius = 0.8 * parent.Radius,
+                Life = game.Life.CreateDependentSource(),
+                Generation = parent.Generation + 1,
+                Hue = parent.Hue + game.Rng.NextDouble() * 0.4,
+                Vel = parent.Vel + 30 * new Vector(Math.Cos(theta), Math.Sin(theta))
+            };
+
+            // if the ball's parent dies, kill it after a small delay
+            parent.Life.Lifetime
+                .WhenAfterLife(() => game.Delay(TimeSpan.FromMilliseconds(100 + game.Rng.NextDouble() * 100)))
+                .WhenDead(ball.Life.EndLifetime);
+            
+            game.Balls.Add(ball, ball.Life.Lifetime);
+            return ball;
+        }
+        public static void StochasticRate(this Game game, double expectedPerSecond, Lifetime lifetime, Action callback) {
+            game.LoopActions.Add(
+                iter => {
+                    // this is just a rough approximation, not exact at all
+                    var rate = expectedPerSecond*iter.dt.TotalSeconds;
+                    var chance = rate;
+                    if (game.Rng.NextDouble() < chance) callback();
+                },
+                lifetime);
+        }
+        public static void SetupPeriodicChildSpawning(this Game game, double expectedPerBallPerSecond, int maxChildrenPerBall, int maxGeneration) {
+            game.Balls.AsObservable().Subscribe(
+                ball => {
+                    if (ball.Value.Generation > maxGeneration) return;
+                    
+                    // keep track of children
+                    var curChildCount = 0;
+                    var children = new PerishableCollection<Ball>();
+                    children
+                        .AsObservable()
+                        .ObserveNonPerishedCount(completeWhenSourceCompletes: true)
+                        .Subscribe(e => curChildCount = e, ball.Lifetime);
+                    
+                    // spawn children periodically at random
+                    game.StochasticRate(
+                        expectedPerBallPerSecond,
+                        ball.Lifetime,
+                        () => {
+                            if (curChildCount >= maxChildrenPerBall) return;
+                            var child = game.SpawnBall(parent: ball.Value);
+                            game.Connectors.Add(new BallLine {Child = child, Parent = ball.Value}, child.Life.Lifetime);
+                            children.Add(child, child.Life.Lifetime);
+                        });
+                },
+                game.Life);
+        }
         public static void SetupMoveAndBounceBalls(this Game game, Func<Rect> playArea) {
             game.LoopActions.Add(
                 iter => {
--- LifetimeExample2/MainWindow.xaml.cs
+++ LifetimeExample2/MainWindow.xaml.cs
@@ -10,64 +10,24 @@ namespace LifetimeExample2 {
         public MainWindow() {
             InitializeComponent();
             this.Loaded += (sender, arg) => {
-                var controls = new PerishableCollection<UIElement>();
-                var game = new Game();
-                controls.AsObservable().Subscribe(
-                    e => {
-                        canvas.Children.Add(e.Value);
-                        e.Lifetime.WhenDead(() => canvas.Children.Remove(e.Value));
-                    },
-                    game.Life);
-                GameLoop(game, controls);
+                RunGame(new Game());
             };
         }
-        private void GameLoop(Game game, PerishableCollection<UIElement> controls) {
+        private void RunGame(Game game) {
+            var controls = new PerishableCollection<UIElement>();
+            controls.AsObservable().Subscribe(
+                e => {
+                    canvas.Children.Add(e.Value);
+                    e.Lifetime.WhenDead(() => canvas.Children.Remove(e.Value));
+                },
+                game.Life);
+
             game.SetupMoveAndBounceBalls(() => new Rect(0, 0, canvas.ActualWidth, canvas.ActualHeight));
             
-            Func<Ball, Ball> spawnBall = null;
-            spawnBall = parent => {
-                var lifeS = new LifetimeSource();
-                parent.Life.Lifetime.WhenDead(() => game.Delay(TimeSpan.FromMilliseconds(100 + game.Rng.NextDouble() * 100)).WhenDead(lifeS.EndLifetime));
-                var ball = new Ball {
-                    Pos = parent.Pos,
-                    Radius = 0.8 * parent.Radius,
-                    Life = lifeS,
-                    Generation = parent.Generation + 1,
-                    Hue = parent.Hue + game.Rng.NextDouble() * 0.4
-                };
-                var life = ball.Life.Lifetime;
-                game.Balls.Add(ball, life);
-
-                var omit = 0.1;
-                var quarter = Math.PI / 2;
-                var theta = game.Rng.NextDouble() * quarter * (1 - 2 * omit)
-                          + quarter * omit
-                          + game.Rng.Next(4) * quarter;
-                ball.Vel = parent.Vel + 30 * new Vector(Math.Cos(theta), Math.Sin(theta));
-                ball.LastPos = ball.Pos;
-
-                // spawn children periodically
-                if (ball.Generation < 4) {
-                    var children = new PerishableCollection<Ball>();
-                    var ex = new LifetimeExchanger();
-                    children.AsObservable().ObserveNonPerishedCount(completeWhenSourceCompletes: true).Subscribe(
-                        e => {
-                            var rx = ex.StartNextAndEndPreviousLifetime();
-                            if (e >= 3) return; // at most 3 children
-                            game.LoopActions.Add(
-                                iter => {
-                                    if (game.Rng.NextDouble() > 0.01) return;
-                                    var s = spawnBall(ball);
-                                    game.Connectors.Add(new BallLine { Child = s, Parent = ball }, s.Life.Lifetime);
-                                    children.Add(s, s.Life.Lifetime);
-                                },
-                                life.Min(rx));
-                        },
-                        life);
-                }
-
-                return ball;
-            };
+            game.SetupPeriodicChildSpawning(
+                expectedPerBallPerSecond: 0.2, 
+                maxChildrenPerBall: 2, 
+                maxGeneration: 5);
 
             game.SetupDrawBallsAsControls(
                 controls, 
@@ -96,8 +56,8 @@ namespace LifetimeExample2 {
                 cutTolerance: 1);
 
 
-            foreach (var repeat in 3.Range()) {
-                spawnBall(new Ball {
+            foreach (var repeat in 5.Range()) {
+                game.SpawnBall(parent: new Ball {
                     Pos = new Point(game.Rng.NextDouble()*canvas.ActualWidth, game.Rng.NextDouble()*canvas.ActualHeight),
                     Radius = 10,
                     Life = new LifetimeSource(),
